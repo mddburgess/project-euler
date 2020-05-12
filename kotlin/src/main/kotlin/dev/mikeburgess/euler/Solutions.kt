5cc60dded569b2beb7dc5ace2100717aafde0567
Mike Burgess <mddburgess@users.noreply.github.com>
2020-05-11 20:01:02 -0700

    Problem 19 - Kotlin

--- kotlin/src/main/kotlin/dev/mikeburgess/euler/Solutions.kt
+++ kotlin/src/main/kotlin/dev/mikeburgess/euler/Solutions.kt
@@ -23,4 +23,5 @@ enum class Solutions(val problem: Problem, val answer: Long) {
     `Problem 016`(Problem016(), 1_366),
     `Problem 017`(Problem017(), 21_124),
     `Problem 018`(Problem018(), 1_074),
+    `Problem 019`(Problem019(), 171),
 }
